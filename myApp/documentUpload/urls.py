6ad25398b6e70ea34225278c18b35752b7cb6a2b
kaanozbudak <kaanozbudak@hotmail.com>
2019-05-02 10:54:36 +0300

    another htmls added

--- myApp/documentUpload/urls.py
+++ myApp/documentUpload/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('', views.upload, name='upload'),
+    path('', views.list_docs, name='list'),
 ]
--- myApp/documentUpload/views.py
+++ myApp/documentUpload/views.py
@@ -1,13 +1,38 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def createContext():
+
+def create_context():
     return {
         'name': 'kaan',
         'surname': 'ozbudak'
     }
 
-def index(request):
-    return render(request, 'documentUpload/index.html', createContext())
-    #return HttpResponse("Hello, world. You're at the document upload pages index.")
 
+def upload_context():
+    return {
+        'name': 'kaan',
+        'surname': 'ozbudak'
+    }
+
+
+def list_context():
+    return {
+        'name': 'kaan',
+        'surname': 'ozbudak'
+    }
+
+
+def index(request):
+    return render(request, 'documentUpload/index.html', create_context())
+    # return HttpResponse("Hello, world. You're at the document upload pages index.")
+
+
+def upload(request):
+    return render(request, 'documentUpload/upload.html', upload_context())
+    # return HttpResponse("Hello, world. You're at the document upload pages index.")
+
+
+def list_docs(request):
+    return render(request, 'documentUpload/list.html', list_context())
+    # return HttpResponse("Hello, world. You're at the document upload pages index.")
