eaf621c7880818faad335e39940e106f0c731723
kaanozbudak <kaanozbudak@hotmail.com>
2019-05-02 09:50:28 +0300

    render html page and add settings app name

--- myApp/documentUpload/views.py
+++ myApp/documentUpload/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
+def createContext():
+    return {
+        'name': 'kaan',
+        'surname': 'ozbudak'
+    }
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'documentUpload/index.html', createContext())
+    #return HttpResponse("Hello, world. You're at the document upload pages index.")
 
