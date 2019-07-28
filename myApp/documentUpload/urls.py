7e04d30d4b7a7a77728258f02af42d9845b79012
kaanozbudak <kaanozbudak@hotmail.com>
2019-07-28 13:18:47 +0300

    files gone

--- myApp/documentUpload/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('', views.upload, name='upload'),
-    path('', views.list_docs, name='list'),
-]
