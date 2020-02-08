4637e8f3c01f170c081b9cf1afed8dcdfbff45df
kaanozbudak <kaanozbudak@hotmail.com>
2020-02-08 21:26:06 +0300

    flex app came

---
+++ mysite/flex/models.py
@@ -0,0 +1,20 @@
+# Create your models here.
+"""Flexible page."""
+
+from django.db import models
+from wagtail.core.models import Page
+from wagtail.admin.edit_handlers import FieldPanel
+
+
+class FlexPage(Page):
+    """Flexible page class"""
+    template = "flex/flex_page.html"
+    subtitle = models.CharField(max_length=100, null=True, blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("subtitle")
+    ]
+
+    class Meta:
+        verbose_name = "Flex Page"
+        verbose_name_plural = "Flex Pages"
